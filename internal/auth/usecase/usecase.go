5e8226923f3f1eefe57b0123d8f2b691dec28f00
adohong4 <adohong4@gmail.com>
2025-07-11 16:05:47 +0700

    feat: add Config

--- internal/auth/usecase/usecase.go
+++ internal/auth/usecase/usecase.go
@@ -1,1 +1,13 @@
 package usecase
+
+import (
+	"github.com/adohong4/driving-license/config"
+	"github.com/adohong4/driving-license/internal/auth"
+	"github.com/adohong4/driving-license/pkg/logger"
+)
+
+type authUC struct {
+	cfg      *config.Config
+	authRepo auth.Repository
+	logger   logger.Logger
+}
