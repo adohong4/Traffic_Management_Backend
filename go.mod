f3c0e19a90bc0f8a6c640b1100a2d4c258a13e51
adohong4 <adohong4@gmail.com>
2025-07-11 15:47:24 +0700

    feat: add pq_repository in auth folder

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.24.4
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/labstack/echo/v4 v4.13.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
--- internal/auth/repository/pg_repository.go
+++ internal/auth/repository/pg_repository.go
@@ -1,1 +1,149 @@
 package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/adohong4/driving-license/internal/auth"
+	"github.com/adohong4/driving-license/internal/models"
+	"github.com/adohong4/driving-license/pkg/utils"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+)
+
+// Auth Repository
+type authRepo struct {
+	db *sqlx.DB
+}
+
+// Auth new constructor
+func NewAuthRepository(db *sqlx.DB) auth.Repository {
+	return &authRepo{db: db}
+}
+
+func (r *authRepo) Register(ctx context.Context, user *models.User) (*models.User, error) {
+	u := &models.User{}
+	if err := r.db.QueryRowxContext(ctx, createUserQuery, &user.IdentityNo, &user.Password, &user.Active, &user.Active, &user.Role,
+		&user.Version, &user.CreatorId, &user.ModifierId, &user.CreatedAt, &user.UpdatedAt).StructScan(u); err != nil {
+		return nil, errors.Wrap(err, "authRepo.Register.StructScan")
+	}
+	return u, nil
+}
+
+func (r *authRepo) Update(ctx context.Context, user *models.User) (*models.User, error) {
+	u := &models.User{}
+	if err := r.db.GetContext(ctx, u, updateUserQuery, &user.IdentityNo, &user.Password, &user.Active, &user.Active, &user.Role,
+		&user.Version, &user.CreatorId, &user.ModifierId, &user.CreatedAt, &user.UpdatedAt, &user.Id); err != nil {
+		return nil, errors.Wrap(err, "authRepo.Update.GetContext")
+	}
+	return u, nil
+}
+
+func (r *authRepo) Delete(ctx context.Context, id uuid.UUID) error {
+
+	result, err := r.db.ExecContext(ctx, deleteUserQuery, id)
+	if err != nil {
+		return errors.Wrap(err, "authRepo.Delete.RowsAffected")
+	}
+	rowAffected, err := result.RowsAffected()
+	if err != nil {
+		return errors.Wrap(err, "authRepo.Delete.RowsAffected")
+	}
+	if rowAffected == 0 {
+		return errors.Wrap(sql.ErrNoRows, "authRepo.Delete.rowsAffected")
+	}
+	return nil
+}
+
+// Get user by id
+func (r *authRepo) GetUserById(ctx context.Context, id uuid.UUID) (*models.User, error) {
+	user := &models.User{}
+	if err := r.db.QueryRowxContext(ctx, getUserQuery, id).StructScan(user); err != nil {
+		return nil, errors.Wrap(err, "authRepo.GetByID.QueryRowxContext")
+	}
+	return user, nil
+}
+
+// Find users by IdentityNO
+func (r *authRepo) FindByIdentityNO(ctx context.Context, name *string, query *utils.PaginationQuery) (*models.UsersList, error) {
+	var totalCount int
+	if err := r.db.GetContext(ctx, &totalCount, getTotalCount, name); err != nil {
+		return nil, errors.Wrap(err, "authRepo.FindByName.GetContext.totalCount")
+	}
+
+	if totalCount == 0 {
+		return &models.UsersList{
+			TotalCount: totalCount,
+			TotalPages: utils.GetTotalPage(totalCount, query.GetSize()),
+			Page:       query.Page,
+			Size:       query.Size,
+			HasMore:    utils.GetHasMore(query.GetPage(), totalCount, query.GetSize()),
+			Users:      make([]*models.User, 0),
+		}, nil
+	}
+
+	rows, err := r.db.QueryxContext(ctx, findUsers, name, query.GetOffset(), query.GetLimit())
+	if err != nil {
+		return nil, errors.Wrap(err, "authRepo.FindByName.QueryxContext")
+	}
+	defer rows.Close()
+
+	var users = make([]*models.User, 0, query.GetSize())
+	for rows.Next() {
+		var user models.User
+		if err = rows.StructScan(&user); err != nil {
+			return nil, errors.Wrap(err, "authRepo.FindByName.StructScan")
+		}
+		users = append(users, &user)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, errors.Wrap(err, "authRepo.FindByName.rows.Err")
+	}
+
+	return &models.UsersList{
+		TotalCount: totalCount,
+		TotalPages: utils.GetTotalPage(totalCount, query.GetPage()),
+		Page:       query.GetPage(),
+		Size:       query.GetSize(),
+		HasMore:    utils.GetHasMore(query.GetPage(), totalCount, query.GetSize()),
+		Users:      users,
+	}, nil
+
+}
+
+// Get user with pagination
+func (r *authRepo) GetUsers(ctx context.Context, pq *utils.PaginationQuery) (*models.UsersList, error) {
+	var totalCount int
+	if err := r.db.GetContext(ctx, &totalCount, getTotal); err != nil {
+		return nil, errors.Wrap(err, "authRepo.GetUsers.GetContext.totalCount")
+	}
+
+	if totalCount == 0 {
+		return &models.UsersList{
+			TotalCount: totalCount,
+			TotalPages: utils.GetTotalPage(totalCount, pq.GetSize()),
+			Page:       pq.GetPage(),
+			Size:       pq.GetSize(),
+			HasMore:    utils.GetHasMore(pq.GetPage(), totalCount, pq.GetSize()),
+			Users:      make([]*models.User, 0),
+		}, nil
+	}
+
+	var users = make([]*models.User, 0, pq.GetSize())
+	if err := r.db.SelectContext(
+		ctx, &users, getUsers, pq.GetOrderBy(), pq.GetOffset(), pq.GetLimit(),
+	); err != nil {
+		return nil, errors.Wrap(err, "authRepo.GetUsers.SelectContext")
+	}
+
+	return &models.UsersList{
+		TotalCount: totalCount,
+		TotalPages: utils.GetTotalPage(totalCount, pq.GetSize()),
+		Page:       pq.GetPage(),
+		Size:       pq.GetSize(),
+		HasMore:    utils.GetHasMore(pq.GetPage(), totalCount, pq.GetSize()),
+		Users:      users,
+	}, nil
+}
