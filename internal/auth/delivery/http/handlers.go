60c4ba546c6a324206cff9d54ab4ae59edf2d392
adohong4 <adohong4@gmail.com>
2025-07-11 21:45:51 +0700

    feat: add handlers

--- internal/auth/delivery/http/handlers.go
+++ internal/auth/delivery/http/handlers.go
@@ -1,1 +1,14 @@
 package http
+
+import (
+	"github.com/adohong4/driving-license/config"
+	"github.com/adohong4/driving-license/internal/auth"
+	"github.com/adohong4/driving-license/pkg/logger"
+)
+
+// Auth handlers
+type authHandlers struct {
+	cfg    *config.Config
+	authUC auth.UseCase
+	logger logger.Logger
+}
